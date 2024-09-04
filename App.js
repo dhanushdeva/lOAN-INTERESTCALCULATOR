31a6c12ef4d77462f4751e2c2dffb3e03b1b413e
DHANUSH D <115514596+dhanushdeva@users.noreply.github.com>
2024-09-04 06:38:23 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import CalculatorSelector from './components/CalculatorSelector';
-import './styles/App.css'; // Import CSS styles
+import './styles/App.css'; 
 
 function App() {
   return (
